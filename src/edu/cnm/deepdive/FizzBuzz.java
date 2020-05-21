e95d8ad866ff45d499c92ea08dec858e9ea9bd14
Paul C <pcutter@cnm.edu>
2020-05-21 14:43:27 -0600

    Decompose methods.

--- src/edu/cnm/deepdive/FizzBuzz.java
+++ src/edu/cnm/deepdive/FizzBuzz.java
@@ -5,20 +5,24 @@ public class FizzBuzz {
   public static void main(String[] args) {
 
     for (int counter = 1; counter <= 100; counter++) {
-String status = "";
-      boolean counterIsFizz = (counter % 3 == 0);
-      boolean counterIsBuzz = (counter % 5 == 0);
-      if (counterIsFizz) {
-        status += "fizz";
-      }
-      if (counterIsBuzz) {
-        status += "buzz";
-      }
-      if (status.isEmpty()) {
-        status += counter;
-      }
-      System.out.println(status);
+      System.out.println(getStatus(counter));
     }
+  }
+
+  static String getStatus(int counter) {
+    String status = "";
+    boolean counterIsFizz = (counter % 3 == 0);
+    boolean counterIsBuzz = (counter % 5 == 0);
+    if (counterIsFizz) {
+      status += "fizz";
+    }
+    if (counterIsBuzz) {
+      status += "buzz";
+    }
+    if (status.isEmpty()) {
+      status += counter;
+    }
+    return status;
   }
 }
 
